0256742db5d33047634881564bbcc0a0c1073352
khanhsonb1c1 <87069555+khanhsonb1c1@users.noreply.github.com>
2022-12-23 17:05:33 +0700

    first commit

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <HomePage/>
+  </div>
+</template>
+<script lang="ts">
+import { defineComponent } from 'vue';
+import HomePage from './views/HomePage.vue';
+export default defineComponent({
+  components:{HomePage}
+})
+</script>
---
+++ src/views/HomePage.vue
@@ -0,0 +1,13 @@
+<template>
+    <Header/>
+    <Slider/>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import Header from "./header/Index.vue";
+import Slider from "./slider/Slider.vue"
+export default defineComponent({
+    components:{Header,Slider}
+});
+</script>
